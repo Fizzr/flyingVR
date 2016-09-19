fcc1dfd10bba66d052ee10b59f1db92dc2a31ae5
Claes <claes.gustav.andersson@gmail.com>
2016-09-19 14:55:19 +0200

    Version 2. Added trees and improved handling

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class PlaneController : MonoBehaviour {
 
-	private static float ROLLMOD= 0.2f;
+	private static float ROLLMOD= 0.2f * 2.8f;
 	private static float PITCHMOD = ROLLMOD;
 	private static float YAWMOD = ROLLMOD * 0.3f;
 	private static float MOTORPOWER = 20f;
 	private static float BANKMOD = 50f;
+	private static float ANGLE_RANGE = 30f;
 
-	public GUIText text;
+	public AudioSource explosionAudio;
 	public Camera cam;
 	private Rigidbody rb;
 	private Vector3 offset;
@@ -30,7 +31,7 @@ public class PlaneController : MonoBehaviour {
 
 		//DEBUG
 
-		rb.AddRelativeTorque(Input.GetAxis("Vertical")*85f*PITCHMOD , 0 , Input.GetAxis("Horizontal")*85f*ROLLMOD*-1f);
+		rb.AddRelativeTorque(Input.GetAxis("Vertical")* ANGLE_RANGE *PITCHMOD , 0 , Input.GetAxis("Horizontal")* ANGLE_RANGE *ROLLMOD*-1f);
 
 		// VR
 		if(!(Input.touchCount > 0))
@@ -51,7 +52,42 @@ public class PlaneController : MonoBehaviour {
 
 			headRot = new Vector3(xrot, yrot, zrot);
 
-			rb.AddRelativeTorque(xrot * PITCHMOD, yrot * YAWMOD, zrot * ROLLMOD);				////// Make more advanced!
+			Vector3 torqueToAdd = new Vector3 (0,0,0);
+
+			if(Mathf.Abs(xrot) >ANGLE_RANGE)
+			{
+				float sign = (xrot > 0? 1: -1);
+				torqueToAdd.x = ANGLE_RANGE * sign;
+			}
+			else
+			{
+				torqueToAdd.x = xrot;
+			}
+			torqueToAdd.x = torqueToAdd.x * PITCHMOD;
+
+			if(Mathf.Abs(yrot) >ANGLE_RANGE)
+			{
+				float sign = (yrot > 0? 1: -1);
+				torqueToAdd.y = ANGLE_RANGE * sign;
+			}
+			else
+			{
+				torqueToAdd.y = yrot;
+			}
+			torqueToAdd.y = torqueToAdd.z * YAWMOD;
+
+			if(Mathf.Abs(zrot) >ANGLE_RANGE)
+			{
+				float sign = (zrot > 0? 1: -1);
+				torqueToAdd.z = ANGLE_RANGE * sign;
+			}
+			else
+			{
+				torqueToAdd.z = zrot;
+			}
+			torqueToAdd.z = torqueToAdd.z * ROLLMOD;
+
+			rb.AddRelativeTorque(torqueToAdd);				////// Make more advanced!
 		}
 		// Lift
 		Vector3 rot = transform.rotation.eulerAngles;
@@ -84,19 +120,17 @@ public class PlaneController : MonoBehaviour {
 			if(roll > 90)
 				roll= Mathf.Abs(roll -180); // 0 to 90, 90 to 0			//Doesn't matter which side, plane is tilting, or if plane is upside down (kinda. simplified)
 			
-			rb.AddForce(0f, -rb.mass*9f * 0.5f * Mathf.Pow(roll/90f, 3f),0f);
-			rb.AddTorque(0, sign * Mathf.Pow(roll/90f, 2f) * BANKMOD, 0);
+			rb.AddForce(0f, -rb.mass*9f * 0.5f * Mathf.Pow(roll/90f, 3f),0f);  //Gravity pulling down
+			rb.AddTorque(0, sign * (roll/90f)/2 * BANKMOD, 0);
 		}
-
-
-
-
-		print ("debug");
-		print ("Velocity: " + vel.ToString());
-		print ("localrot: " + transform.localRotation.eulerAngles.ToString());
-		print ("rotation: " + transform.TransformDirection(transform.localRotation.eulerAngles).ToString());
-		//print ("angular Velocity: " + rb.angularVelocity.ToString ());
 	}
 
+	void OnCollisionEnter(Collision collision)
+	{
+		rb.AddExplosionForce(1000, collision.contacts[0].point,200);
+		print("Collider: "+ collision.gameObject.ToString());
+		explosionAudio.transform.position = collision.contacts[0].point;
+		explosionAudio.Play();
+	}
 
 }
